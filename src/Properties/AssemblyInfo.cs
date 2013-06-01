771fb5c7a28f6222b51001a1e3d762d6f135912f
shane <shane@anderson.geek.nz>
2013-06-01 17:08:44 +1200

    initial project structure

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Arraybracket.TypedViews")]
+[assembly: AssemblyProduct("Arraybracket.TypedViews")]
+[assembly: AssemblyDescription("Placeholder placeholder")]
+[assembly: AssemblyCompany("Arraybracket Limited")]
+[assembly: AssemblyCopyright("Copyright ©2013 Arraybracket Limited")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
